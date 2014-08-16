c5250bc9e1637855aba7c0081978fd322ed2dcf3
MIR <mightykiwi@yahoo.com>
2014-08-16 10:54:54 -0700

    initial commit

---
+++ spec/spec_helper.rb
@@ -0,0 +1,16 @@
+require 'rspec'
+require 'pg'
+require 'pry'
+require 'comic'
+require 'character'
+
+DB = PG.connect({:dbname => 'comic_test'})
+
+RSpec.configure do |config|
+  config.before(:each) do
+    DB.exec("DELETE FROM comics *;")
+    DB.exec("DELETE FROM characters *;")
+    DB.exec("DELETE FROM comic_characters *;")
+  end
+end
+
