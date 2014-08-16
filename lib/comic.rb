ffd08075853b0cc6fdd2899b36cf07e20d0a9110
MIR <mightykiwi@yahoo.com>
2014-08-16 12:09:35 -0700

    Comic: add save/list

--- lib/comic.rb
+++ lib/comic.rb
@@ -1,9 +1,24 @@
 class Comic
 
-  attr_accessor :name
+  attr_accessor :name, :id
 
   def initialize attributes
-    @name = attributes[:name]
+    @name = attributes['name']
+    @id = attributes['id']
+  end
+
+  def self.all
+    all_comics = []
+    results = DB.exec("SELECT * FROM comics;")
+    results.each do |result|
+      all_comics << Comic.new(result)
+    end
+    all_comics
+  end
+
+  def save
+    @id = DB.exec("INSERT INTO comics (name) VALUES ('#{@name}') RETURNING id;").first['id'].to_i
   end
 
 end
+
--- spec/comic_spec.rb
+++ spec/comic_spec.rb
@@ -4,9 +4,17 @@ describe Comic do
 
   describe 'initialize' do
     it 'initializes comic book with name' do
-      avengers = Comic.new({name: 'Avengers'})
+      avengers = Comic.new({'name' => 'Avengers'})
       expect(avengers).to be_a Comic
       expect(avengers.name).to eq 'Avengers'
+    end
+  end
+
+  describe 'all' do
+    it 'lists all saved instances of comic' do
+      avengers = Comic.new({'name' => 'Avengers'})
+      avengers.save
+      expect(Comic.all[0].id.to_i).to eq avengers.id
     end
   end
 
